264617e3c9c0cd886203e108a6d18b79a2824a61
Svetlana <pidgin66@gmail.com>
2024-07-22 10:13:24 +0300

    add knex

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AdminUploadPage from './components/AdminUploadPage.jsx';
+
+const App = () => (
+    <Router>
+      <Routes>
+        <Route path="/admin/upload" element={<AdminUploadPage  />} />
+        {/* Другие маршруты */}
+      </Routes>
+    </Router>
+);
+
+export default App;
