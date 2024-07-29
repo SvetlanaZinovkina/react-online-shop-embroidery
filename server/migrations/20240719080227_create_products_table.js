7c42f583a95bc267cd966af3d07de28cb4d92152
SvetlanaZinovkina <75115284+SvetlanaZinovkina@users.noreply.github.com>
2024-07-29 10:16:46 +0000

    add sales for product migration options

--- server/migrations/20240719080227_create_products_table.js
+++ server/migrations/20240719080227_create_products_table.js
@@ -5,6 +5,8 @@ export const up = (knex) => (
 				table.text('description');
 				table.decimal('price', 10, 2).notNullable();
 				table.string('file_path', 255).notNullable();
+				table.boolean('is_on_sale').defaultTo(false);
+				table.decimal('sale_price', 10, 2).nullable();
 				table.timestamps(true, true);
 		})
 );
