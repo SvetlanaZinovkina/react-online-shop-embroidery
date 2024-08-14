d1be6229aa6de83c3d6693a548c14195590ac02e
Svetlana <pidgin66@gmail.com>
2024-08-14 11:34:58 +0300

    fix 404 page

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router';
 import { Button } from 'react-bootstrap';
 import routes from '../routes/routes.js';
 
 const Header = ({ children }) => {
   const { t } = useTranslation();
-  const navigate = useNavigate();
 
   return (
     <header className="hero-section">
--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Image } from 'react-bootstrap';
-import notFoundImage from '../images/notFoundImage.jpg';
+import {
+  Container, Row, Col,
+} from 'react-bootstrap';
+import Navigate from '../components/navbar.jsx';
+import Footer from '../components/footer.jsx';
 
 const NotFound = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="d-flex flex-column justify-content-center align-items-center vh-100 position-relative bg-dark">
-      <Image
-        alt={t('notFound.notPage')}
-        src={notFoundImage}
-        className="position-absolute w-100 h-100 object-fit-cover"
-        style={{ zIndex: -1, opacity: 0.7 }}
-      />
-      <div className="text-center text-white p-3" style={{ zIndex: 1 }}>
-        <h1 className="display-1 fw-bold">404</h1>
-        <h4 className="mb-3">{t('notFound.notPage')}</h4>
-        <p className="lead">
-          {t('notFound.mainLink')}{' '}
-          <a href="/" className="text-info text-decoration-none fw-semibold">
-            {t('notFound.textMuted')}
-          </a>
-        </p>
-      </div>
+    <div className="d-flex flex-column min-vh-100">
+      <Navigate />
+      <Container
+        fluid
+        className="d-flex flex-column justify-content-center align-items-center flex-grow-1 bg-light p-0"
+      >
+        <Row className="text-center">
+          <Col>
+            <div>
+              <h1 className="display-1 fw-bold">404</h1>
+              <h4 className="mb-3">{t('notFound.notPage')}</h4>
+              <p className="lead">
+                {t('notFound.mainLink')}
+                {' '}
+                <a href="/" className="btn btn-link">
+                  {t('notFound.textMuted')}
+                </a>
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <Footer className="bg-light" />
     </div>
   );
 };
