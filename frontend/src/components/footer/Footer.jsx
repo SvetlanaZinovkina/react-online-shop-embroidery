77706b7dc22bbac25601e2404ab44440dd75b84c
Svetlana Zinovkina <75115284+SvetlanaZinovkina@users.noreply.github.com>
2024-08-19 10:33:32 +0000

    scss for header

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,32 +1,5 @@
-// import React from 'react';
-// import { useTranslation } from 'react-i18next';
-// import { Container, Row, Col } from 'react-bootstrap';
-// import routes from '../../routes/routes.js';
-// import styles from './Footer.module.scss';
-//
-// const Footer = () => {
-//   const { t } = useTranslation();
-//   return (
-//     <footer className={styles.footer}>
-//       <Container>
-//         <Row>
-//           <Col md={6} xs={12} className={styles.contact}>
-//             <h5>{t('footer.contacts')}</h5>
-//             <p>{t('footer.phoneNumber')}</p>
-//             <p>{t('footer.email')}</p>
-//             <p>{t('footer.address')}</p>
-//             <a href={routes.policy()} className="link-dark">{t('footer.policy')}</a>
-//           </Col>
-//           <Col md={6} xs={12} className={styles['footer-rights-image']} />
-//         </Row>
-//       </Container>
-//     </footer>
-//   );
-// };
-//
-// export default Footer;
-
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import styles from './Footer.module.scss';
 import routes from '../../routes/routes.js';
@@ -43,7 +16,7 @@ const Footer = () => {
               <p>{t('footer.phoneNumber')}</p>
               <p>{t('footer.email')}</p>
               <p>{t('footer.address')}</p>
-              <a href={routes.policy()} className={styles.link}>{t('footer.policy')}</a>
+              <Link to={routes.policy()} className={styles.link}>{t('footer.policy')}</Link>
             </div>
           </section>
           <figure className={styles.col}>
--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,36 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import {
-  Button, Image, Container, Row, Col,
-} from 'react-bootstrap';
 import routes from '../../routes/routes.js';
-import { useGetPopularEmbroideryQuery } from '../../store/api.js';
-import nike from '../../assets/images/bear.gif';
 import styles from './header.module.scss';
+import nike from '../../assets/images/bear.gif';
 
 const Header = () => {
   const { t } = useTranslation();
 
   return (
-    <header className="hero-section">
-      <Container fluid>
-        <Row className="align-items-center justify-content-center">
-          <Col md={6} xs={12} className="d-flex align-items-start justify-content-sm-around flex-column">
-            <h1>{t('mainPage.welcome')}</h1>
-            <Button variant="light" href={routes.shopPage()} className="mt-3">
-              {t('mainPage.catalog')}
-            </Button>
-          </Col>
-          <Col md={6} xs={12} className="d-flex">
-            <Image
-              src={nike}
-              alt={t('navBar.nameShop')}
-              height="450"
-              className="img-fluid"
-            />
-          </Col>
-        </Row>
-      </Container>
+    <header className={styles['hero-section']}>
+      <div className={styles.container}>
+        <section className={`${styles.col} ${styles['left-column']}`}>
+          <h1>{t('mainPage.welcome')}</h1>
+          <Link to={routes.shopPage()} className={styles.button}>
+            {t('mainPage.catalog')}
+          </Link>
+        </section>
+        <aside className={`${styles.col} ${styles['right-column']}`}>
+          <img
+            src={nike}
+            alt={t('navBar.nameShop')}
+          />
+        </aside>
+      </div>
     </header>
   );
 };
