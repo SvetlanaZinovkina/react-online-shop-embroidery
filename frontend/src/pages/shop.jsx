a422902681edcde09a9f1a2572cc429c35f44d9a
Svetlana <pidgin66@gmail.com>
2024-08-21 14:31:02 +0300

    fix config for test

---
+++ frontend/src/pages/shop.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Shop = () => (
+  <div>
+    <h1>Shop Page</h1>
+    {/* Ваш контент */}
+  </div>
+);
+
+export default Shop;
---
+++ frontend/src/pages/userCabinet.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const UserCabinet = () => (
+  <div>
+    <h1>User Cabinet</h1>
+    {/* Ваш контент */}
+  </div>
+);
+
+export default UserCabinet;
